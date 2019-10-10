3d51765d7be63b299c0cb095dd3d03b1d00b03bf
John Plevyak <jplevyak@gmail.com>
2019-10-10 13:04:14 -0700

    Merge pull request #111 from jplevyak/release-1.3-use-after-free
    Apply fix for use-after-free in Envoy ThreadLocal Slot.
    
    Signed-off-by: John Plevyak <jplevyak@gmail.com>

--- source/common/config/config_provider_impl.h
+++ source/common/config/config_provider_impl.h
@@ -369,8 +369,12 @@ public:
     if (getConfig() == config) {
       return;
     }
-    tls_->runOnAllThreads(
-        [this, config]() -> void { tls_->getTyped<ThreadLocalConfig>().config_ = config; });
+    tls_->runOnAllThreads([config](ThreadLocal::ThreadLocalObjectSharedPtr previous)
+                              -> ThreadLocal::ThreadLocalObjectSharedPtr {
+      auto prev_thread_local_config = std::dynamic_pointer_cast<ThreadLocalConfig>(previous);
+      prev_thread_local_config->config_ = config;
+      return previous;
+    });
   }
 
 protected:
